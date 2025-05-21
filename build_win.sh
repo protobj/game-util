280d593c1532e9c808d9d4013dbedd80df4414ea
chenqiang <3146330150@qq.com>
2025-05-21 17:52:08 +0800

    wind打包

--- build_win.sh
+++ build_win.sh
@@ -1,4 +1,4 @@
 export CC_x86_64_pc_windows_gnu="x86_64-w64-mingw32-gcc"
 export CXX_x86_64_pc_windows_gnu="x86_64-w64-mingw32-g++"
 export AR_x86_64_pc_windows_gnu="x86_64-w64-mingw32-ar"
-cargo build --target x86_64-pc-windows-gnu --release
+cargo bundle --target x86_64-pc-windows-gnu --release
